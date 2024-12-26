4e6583dad58edfc91a0d65616225456b11368533
pb <50667687+bereilhp@users.noreply.github.com>
2024-12-26 11:19:44 -0300

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+import { GeistSans } from "geist/font/sans"
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body>{children}</body>
+    </html>
+  )
+}
